5c920d949eca7329e6227493bd8e8df2741508b2
sarassaura <sarahyukinonakada@gmail.com>
2024-08-04 22:34:50 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <img
+        src="https://i.imgur.com/H88yIo2.png"
+        alt="Imagem Tela Inicial"
+        className="img"
+      />
+    </>
+  );
+}
+
+export default Home;
