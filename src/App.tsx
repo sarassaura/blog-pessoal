edbade38ea74b387c7cd4b6e2bbc6fdef355bdd4
sarassaura <sarahyukinonakada@gmail.com>
2024-08-06 13:20:03 -0300

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import Grid from "./components/Grid/Grid";
-
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Layout from "./components/Layout";
 function App() {
   return (
     <>
-      <Grid />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
 export default App;
