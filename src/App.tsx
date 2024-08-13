c01eb1755db7c1ca644a9557601410a8fff474a3
sarassaura <sarahyukinonakada@gmail.com>
2024-08-13 02:38:47 -0300

    feat: implement theme

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import Login from "./pages/Login";
 import Layout from "./components/Layout";
 import Cadastro from "./pages/Cadastro";
 import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./components/temas/ListaTemas";
+import FormularioTema from "./components/temas/FormularioTema";
+import DeletarTema from "./components/temas/DeletarTema";
+
 function App() {
   return (
     <>
@@ -14,10 +18,14 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
+                <Route index element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/temas" element={<ListaTemas />} />
+                <Route path="/cadastroTema" element={<FormularioTema />} />
+                <Route path="/editarTema/:id" element={<FormularioTema />} />
+                <Route path="/deletarTema/:id" element={<DeletarTema />} />
               </Route>
             </Routes>
           </BrowserRouter>
